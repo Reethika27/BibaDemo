4beeeacda2ea8ce048b49b6370cf4c042916e434
Reethika27 <51917721+Reethika27@users.noreply.github.com>
2021-03-22 15:11:01 +0530

    Update NewCollections.js

--- src/Body/NewCollections.js
+++ src/Body/NewCollections.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Card, Row, Col,CardImg} from 'reactstrap';
 import './text.css';
+import mnc from './Images/mnc.jpg';
+import athlei from './Images/athlei.jpg';
+import fun  from './Images/fun.jpg';
+import rts from './Images/RTS.jpg';
+import foot from './Images/foot.jpg';
+import jewel  from './Images/jewel.jpg';
 function NewCollections()
 {
     const style = {
@@ -21,29 +27,29 @@ function NewCollections()
                  <br/>
                 <Row >
                     <Col  >
-                       <a href="#"> <CardImg  height="550px"   src="https://storage.sg.content-cdn.io/in-resources/6c57599f-2c43-4c82-806a-e07c3410f5d3/Images/userimages/home-page-banners/1-2-2021/maskglass.jpg"></CardImg></a>
+                       <a href="#"> <CardImg  height="550px"   src={mnc}></CardImg></a>
                        <div class="centered" ><h1 style={{fontSize:"30px"}}>MASK & CHAIN</h1></div>
                     </Col>
                     <Col>
-                        <a href="#"><CardImg height="550px"  src="https://storage.sg.content-cdn.io/in-resources/6c57599f-2c43-4c82-806a-e07c3410f5d3/Images/userimages/home-page-banners/1-2-2021/athleisure.jpg"></CardImg></a>
+                        <a href="#"><CardImg height="550px"  src={athlei}></CardImg></a>
                         <div class="centered1" ><h1 style={{fontSize:"30px"}}>ATHLEISURE</h1></div>
                     </Col>
                     <Col >
-                        <a href="#"><CardImg height="550px"  src="https://storage.sg.content-cdn.io/in-resources/6c57599f-2c43-4c82-806a-e07c3410f5d3/Images/userimages/home-page-banners/1-2-2021/girlsb.jpg"></CardImg></a>
+                        <a href="#"><CardImg height="550px"  src={fun}></CardImg></a>
                         <div class="centered" ><h1 style={{fontSize:"30px"}}>FUN CASUALS</h1></div>
                     </Col>
                 </Row>
                 <Row >
                     <Col >
-                       <a href="#"> <CardImg  height="500px"   src="https://storage.sg.content-cdn.io/in-resources/6c57599f-2c43-4c82-806a-e07c3410f5d3/Images/userimages/home-page-banners/1-3-21/rts.jpg"></CardImg></a>
+                       <a href="#"> <CardImg  height="500px"   src={rts}></CardImg></a>
                        <div class="centered" ><h1 style={{fontSize:"30px"}}>READY TO STITCH</h1></div>
                     </Col>
                     <Col>
-                        <a href="#"><CardImg height="500px"  src="https://storage.sg.content-cdn.io/in-resources/6c57599f-2c43-4c82-806a-e07c3410f5d3/Images/userimages/home-page-banners/1-3-21/footwear.jpg"></CardImg></a>
+                        <a href="#"><CardImg height="500px"  src={foot}></CardImg></a>
                         <div class="centered1" ><h1 style={{fontSize:"30px"}}>FOOTWEAR</h1></div>
                     </Col>
                     <Col>
-                        <a href="#"><CardImg height="500px"  src="https://storage.sg.content-cdn.io/in-resources/6c57599f-2c43-4c82-806a-e07c3410f5d3/Images/userimages/home-page-banners/1-3-21/jewellery.jpg"></CardImg></a>
+                        <a href="#"><CardImg height="500px"  src={jewel}></CardImg></a>
                         <div class="centered" ><h1 style={{fontSize:"30px"}}>JEWELLERY</h1></div>
                     </Col>
                 </Row>

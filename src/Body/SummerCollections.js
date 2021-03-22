e7f3ad811c60c66c4369c0b5bc1c29b011f49f3d
Reethika27 <51917721+Reethika27@users.noreply.github.com>
2021-03-22 12:18:56 +0530

    Update SummerCollections.js

--- src/Body/SummerCollections.js
+++ src/Body/SummerCollections.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Card, Row, Col,CardImg} from 'reactstrap';
+import './text.css';
 function SummerCollection()
 {
+      const style = {
+        fontWeight:"Bold",
+        fontStyle:"sans-serif" ,
+        color:"black",
+        display: "block",
+        align:"center",
+        fontWeight:"20px"
+      };
     return (
       
         <>
@@ -11,22 +20,27 @@ function SummerCollection()
                  </Row>
                  <br/>
                 <Row >
-                    <Col >
-                       <a href="#"> <CardImg  height="550px"   src="https://storage.sg.content-cdn.io/in-resources/6c57599f-2c43-4c82-806a-e07c3410f5d3/Images/userimages/home-page-banners/1-2-2021/suit4.jpg"></CardImg></a>
+                    <Col class ="container">
+                       <a href="#"> <CardImg  height="550px"   src="https://storage.sg.content-cdn.io/in-resources/6c57599f-2c43-4c82-806a-e07c3410f5d3/Images/userimages/home-page-banners/1-2-2021/suit4.jpg"></CardImg>
+                       <div class="centered" ><h1 style={{fontSize:"30px"}}>SUIT SETS</h1></div>
+                       </a>
                     </Col>
-                    <Col>
+                    <Col  class ="container">
                         <a href="#"><CardImg height="550px"  src="https://storage.sg.content-cdn.io/in-resources/6c57599f-2c43-4c82-806a-e07c3410f5d3/Images/userimages/home-page-banners/1-2-2021/kurta4.jpg"></CardImg></a>
+                        <div class="centered1" ><h1 style={{fontSize:"30px"}}>KURTAS</h1></div>
                     </Col>
-                    <Col >
+                    <Col   class ="container">
                         <a href="#"><CardImg height="550px"  src="https://storage.sg.content-cdn.io/in-resources/6c57599f-2c43-4c82-806a-e07c3410f5d3/Images/userimages/home-page-banners/1-2-2021/dress4.jpg"></CardImg></a>
+                        <div class="centered" ><h1 style={{fontSize:"30px"}}>INDIE DRESSES</h1></div>
                     </Col>
                 </Row>
                 <Row >
-                    <Col >
-                       <a href="#"> <CardImg  height="500px"   src="https://storage.sg.content-cdn.io/in-resources/6c57599f-2c43-4c82-806a-e07c3410f5d3/Images/userimages/home-page-banners/1-2-2021/anar4.jpg"></CardImg></a>
+                    <Col  class ="container">  
+                     <a href="#"> <CardImg  height="500px"   src="https://storage.sg.content-cdn.io/in-resources/6c57599f-2c43-4c82-806a-e07c3410f5d3/Images/userimages/home-page-banners/1-2-2021/anar4.jpg"></CardImg></a>
                     </Col>
-                    <Col>
+                    <Col  class ="container">
                         <a href="#"><CardImg height="500px"  src="https://storage.sg.content-cdn.io/in-resources/6c57599f-2c43-4c82-806a-e07c3410f5d3/Images/userimages/home-page-banners/1-2-2021/bottom4.jpg"></CardImg></a>
+                       
                     </Col>
                 </Row>
                     
